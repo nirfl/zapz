bb37fa5e9968b0fd2aed5cd9b60a4df52b7dfe52
nirfl <nir.flato@gmail.com>
2023-09-29 20:04:13 +0300

    2nd commit

--- myfirstproject/client.py
+++ myfirstproject/client.py
@@ -1,18 +1,24 @@
 import requests
 import getpass
-
+import time
 
 def main():
     print("welcome", getpass.getuser())
     nirurl = "http://10.0.0.25:5000/"
     michalurl = "http://10.0.0.27:5000/"
     chose = int(input("Nir(1) or Michal(2)?"))
-    if chose == 1:
-        rr = requests.get(url=nirurl)
+    chose2 = int(input("are you happy? (1)yes (2)no"))
+    while True:
+        if chose == 1:
+            url = nirurl
+        else:
+            url = michalurl
+        if chose2 == 1:
+            rr = requests.get(url=url)
+        else:
+            rr = requests.get(url=url+"user", params={"user": getpass.getuser()})
         print(rr.json())
-    elif chose == 2:
-        rr = requests.get(url=michalurl)
-        print(rr.json())
+        time.sleep(2)
 
 
 
--- myfirstproject/server.py
+++ myfirstproject/server.py
@@ -1,10 +1,19 @@
-from flask import Flask,jsonify
+from flask import Flask, jsonify, request
+import getpass
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return jsonify('Hello, Michal1')
+
+@app.route("/user", methods=["GET"])
+def get_user():
+    args = request.args
+    print(args)
+    user = args["user"]
+    print(user)
+    return jsonify(f"hello {user}. You are connected to {getpass.getuser()}")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
