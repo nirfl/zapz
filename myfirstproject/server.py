9e2975f1553b917d34e1f686da6b90fd8e8f4465
nirfl <nir.flato@gmail.com>
2023-09-29 20:21:01 +0300

    3rd commit

--- myfirstproject/server.py
+++ myfirstproject/server.py
@@ -1,17 +1,29 @@
 from flask import Flask, jsonify, request
 import getpass
+import datetime
+
+
+
 
 app = Flask(__name__)
 
+connected_users ={}
+
 @app.route('/')
 def hello_world():
-    return jsonify('Hello, Michal1')
-
+    result = "logged users:\n"
+    for user in connected_users:
+        formatted_datetime = connected_users[user].strftime("%Y-%m-%d %H:%M:%S")
+        s = f"{user} logged in time:{formatted_datetime}"
+        result+= s+"\n"
+    return jsonify(result)
 @app.route("/user", methods=["GET"])
 def get_user():
+    current_datetime = datetime.datetime.now()
     args = request.args
     print(args)
     user = args["user"]
+    connected_users[user] = current_datetime
     print(user)
     return jsonify(f"hello {user}. You are connected to {getpass.getuser()}")
 
