05987126cfccdbd565b91b3fd3fb21349c10d070
nirfl <nir.flato@gmail.com>
2023-09-29 19:41:29 +0300

    2nd commit

--- myfirstproject/client.py
+++ myfirstproject/client.py
@@ -3,11 +3,17 @@ import getpass
 
 
 def main():
-    print(getpass.getuser())
+    print("welcome", getpass.getuser())
     nirurl = "http://10.0.0.25:5000/"
     michalurl = "http://10.0.0.27:5000/"
-    rr = requests.get(url=michalurl)
-    print(rr.json())
+    chose = int(input("Nir(1) or Michal(2)?"))
+    if chose == 1:
+        rr = requests.get(url=nirurl)
+        print(rr.json())
+    elif chose == 2:
+        rr = requests.get(url=michalurl)
+        print(rr.json())
+
 
 
 if __name__ == '__main__':
